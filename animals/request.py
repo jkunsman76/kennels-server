528d2f775e53ef64edd612b3b59bf3e4d461e1c4
jkuns76 <jkoonman@gmail.com>
2022-01-10 15:32:08 -0600

    first commit

---
+++ animals/request.py
@@ -0,0 +1,463 @@
+
+ANIMALS = [
+    {
+        "id": 1,
+        "name": "Doodles",
+        "breed": "German Shepherd",
+        "locationId": 1
+    },
+    {
+        "id": 3,
+        "name": "Angus",
+        "breed": "Dalmatian 👾",
+        "locationId": 1
+    },
+    {
+        "id": 4,
+        "name": "Henley",
+        "breed": "Carolina Retriever 🚒",
+        "locationId": 1
+    },
+    {
+        "id": 5,
+        "name": "Derkins",
+        "breed": "Shih tzu 👿",
+        "locationId": 2
+    },
+    {
+        "id": 6,
+        "name": "Checkers",
+        "breed": "Bulldog",
+        "locationId": 1
+    },
+    {
+        "name": "Sawyer",
+        "breed": "Lollie",
+        "id": 7,
+        "locationId": 2
+    },
+    {
+        "name": "Gypsy",
+        "breed": "Miniature Schnauzer",
+        "id": 8,
+        "locationId": 1
+    },
+    {
+        "name": "Zipper",
+        "breed": "Terrier",
+        "locationId": 2,
+        "id": 9
+    },
+    {
+        "name": "Blue",
+        "breed": "Hound dog",
+        "locationId": 2,
+        "id": 10
+    },
+    {
+        "name": "Bugle",
+        "breed": "Beagle",
+        "locationId": 1,
+        "id": 11
+    },
+    {
+        "name": "pohtaytoe",
+        "breed": "mutt",
+        "employeeId": 3,
+        "locationId": None,
+        "id": 12
+    }
+],
+TREATMENTS = [
+    {
+        "id": 1,
+        "animalId": 3,
+        "timestamp": 1634687829221,
+        "description": "Nails were trimmed"
+    },
+    {
+        "animalId": 6,
+        "timestamp": "12/2/2021, 3:23:48 PM",
+        "description": "snip snip ",
+        "id": 3
+    },
+    {
+        "animalId": 11,
+        "timestamp": "12/2/2021, 3:29:58 PM",
+        "description": "snip snip ",
+        "id": 5
+    },
+    {
+        "animalId": 1,
+        "timestamp": "12/2/2021, 3:58:38 PM",
+        "description": "New Treatment Test",
+        "id": 6
+    },
+    {
+        "animalId": 11,
+        "timestamp": "12/3/2021, 10:17:47 AM",
+        "description": "snip snip ",
+        "id": 7
+    }
+],
+ANIMAL_CARETAKERS = [
+    {
+        "id": 1,
+        "animalId": 1,
+        "userId": 5
+    },
+    {
+        "id": 2,
+        "animalId": 5,
+        "userId": 9
+    },
+    {
+        "id": 3,
+        "animalId": 3,
+        "userId": 2
+    },
+    {
+        "id": 4,
+        "animalId": 4,
+        "userId": 14
+    },
+    {
+        "id": 5,
+        "animalId": 5,
+        "userId": 12
+    },
+    {
+        "id": 6,
+        "animalId": 6,
+        "userId": 7
+    },
+    {
+        "id": 6,
+        "animalId": 7,
+        "userId": 5
+    },
+    {
+        "id": 8,
+        "animalId": 8,
+        "userId": 1
+    },
+    {
+        "id": 9,
+        "animalId": 9,
+        "userId": 11
+    },
+    {
+        "id": 10,
+        "animalId": 10,
+        "userId": 4
+    },
+    {
+        "id": 11,
+        "animalId": 11,
+        "userId": 3
+    },
+    {
+        "id": 12,
+        "animalId": 3,
+        "userId": 14
+    },
+    {
+        "animalId": 1,
+        "userId": 1,
+        "id": 13
+    },
+    {
+        "animalId": 4,
+        "userId": 2,
+        "id": 14
+    }
+],
+USERS = [
+    {
+        "id": 1,
+        "name": "Jessica Younker",
+        "email": "jessica@younker.com",
+        "employee": True
+    },
+    {
+        "id": 2,
+        "name": "Jordan Nelson",
+        "email": "jordan@nelson.com",
+        "employee": True
+    },
+    {
+        "id": 3,
+        "name": "Zoe LeBlanc",
+        "email": "zoe@leblanc.com",
+        "employee": True
+    },
+    {
+        "name": "Meg Ducharme",
+        "email": "meg@ducharme.com",
+        "id": 4,
+        "employee": True
+    },
+    {
+        "name": "Hannah Hall",
+        "email": "hannah@hall.com",
+        "id": 5,
+        "employee": True
+    },
+    {
+        "name": "Emily Lemmon",
+        "email": "emily@lemmon.com",
+        "id": 6,
+        "employee": True
+    },
+    {
+        "name": "Jordan Castelloe",
+        "email": "jordan@castelloe.com",
+        "id": 7,
+        "employee": True
+    },
+    {
+        "name": "Leah Gwin",
+        "email": "leah@gwin.com",
+        "id": 8,
+        "employee": True
+    },
+    {
+        "name": "Caitlin Stein",
+        "email": "caitlin@stein.com",
+        "id": 9,
+        "employee": True
+    },
+    {
+        "name": "Greg Korte",
+        "email": "greg@korte.com",
+        "id": 10,
+        "employee": True
+    },
+    {
+        "name": "Charisse Lambert",
+        "email": "charisse@lambert.com",
+        "id": 11,
+        "employee": True
+    },
+    {
+        "name": "Madi Peper",
+        "email": "madi@peper.com",
+        "id": 12,
+        "employee": True
+    },
+    {
+        "name": "Jenna Solis",
+        "email": "jenna@solis.com",
+        "id": 14,
+        "employee": True
+    },
+    {
+        "id": 15,
+        "name": "Ryan Tanay",
+        "email": "ryan@tanay.com",
+        "employee": False
+    },
+    {
+        "id": 16,
+        "name": "Emma Beaton",
+        "email": "emma@beaton.com",
+        "employee": False
+    },
+    {
+        "id": 17,
+        "name": "Dani Adkins",
+        "email": "dani.adkins.com",
+        "employee": False
+    },
+    {
+        "id": 18,
+        "name": "Adam Oswalt",
+        "email": "adam@oswalt.com",
+        "employee": False
+    },
+    {
+        "id": 19,
+        "name": "Fletcher Bangs",
+        "email": "flangs@bangs.com",
+        "employee": False
+    },
+    {
+        "id": 20,
+        "name": "Angela Lee",
+        "email": "lee@lee.com",
+        "employee": False
+    },
+    {
+        "name": "mike mike",
+        "email": "m@m.com",
+        "employee": False,
+        "id": 21
+    },
+    {
+        "name": "Eric \"Macho Man\" Taylor",
+        "email": "macho@man.com",
+        "employee": True,
+        "id": 22
+    },
+    {
+        "name": "Jacob R Kunsman",
+        "employee": True,
+        "email": "jkoonman@gmail.com",
+        "id": 23
+    },
+    {
+        "name": "Jacob Kunsman",
+        "email": "jkoonman@gmail.com",
+        "employee": False,
+        "locationId": 0,
+        "id": 24
+    }
+],
+EMPLOYEE_LOCATIONS = [
+    {
+        "id": 1,
+        "userId": 1,
+        "locationId": 1
+    },
+    {
+        "id": 2,
+        "userId": 2,
+        "locationId": 2
+    },
+    {
+        "id": 15,
+        "userId": 2,
+        "locationId": 1
+    },
+    {
+        "id": 3,
+        "userId": 3,
+        "locationId": 1
+    },
+    {
+        "id": 4,
+        "userId": 4,
+        "locationId": 2
+    },
+    {
+        "id": 5,
+        "userId": 5,
+        "locationId": 2
+    },
+    {
+        "id": 6,
+        "userId": 6,
+        "locationId": 2
+    },
+    {
+        "id": 7,
+        "userId": 7,
+        "locationId": 1
+    },
+    {
+        "id": 8,
+        "userId": 8,
+        "locationId": 1
+    },
+    {
+        "id": 9,
+        "userId": 9,
+        "locationId": 2
+    },
+    {
+        "id": 10,
+        "userId": 10,
+        "locationId": 1
+    },
+    {
+        "id": 11,
+        "userId": 11,
+        "locationId": 1
+    },
+    {
+        "id": 12,
+        "userId": 12,
+        "locationId": 2
+    },
+    {
+        "id": 14,
+        "userId": 14,
+        "locationId": 2
+    },
+    {
+        "userId": 23,
+        "locationId": 2,
+        "id": 16
+    }
+],
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South"
+    }
+],
+ANIMAL_OWNERS = [
+    {
+        "id": 1,
+        "animalId": 1,
+        "userId": 15
+    },
+    {
+        "id": 3,
+        "animalId": 3,
+        "userId": 16
+    },
+    {
+        "id": 4,
+        "animalId": 4,
+        "userId": 17
+    },
+    {
+        "id": 5,
+        "animalId": 5,
+        "userId": 18
+    },
+    {
+        "id": 6,
+        "animalId": 5,
+        "userId": 19
+    },
+    {
+        "animalId": 6,
+        "userId": 20,
+        "id": 7
+    },
+    {
+        "animalId": 11,
+        "userId": 17,
+        "id": 8
+    },
+    {
+        "animalId": 3,
+        "userId": 16,
+        "id": 9
+    },
+    {
+        "animalId": 4,
+        "userId": 18,
+        "id": 10
+    },
+    {
+        "animalId": 1,
+        "userId": 15,
+        "id": 11
+    },
+    {
+        "animalId": 7,
+        "userId": 21,
+        "id": 12
+    }
+]
+
+
+def get_all_animals():
+    return ANIMALS
---
+++ locations/request.py
@@ -0,0 +1,15 @@
+
+
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South"
+    }
+],
+
+def get_all_locations():
+    return LOCATIONS
